eed9f055f7416662473e380e72c7cc655560a7ac
Ahmed Ibrahim <eng.ahmedibrahim21@gmail.com>
2023-09-01 21:37:13 +0300

    Toggle Led function

---
+++ Unit_3_Embedded_C/Lesson_1_Assignment2/Led_Toogle/Debug/Src/main.d
@@ -0,0 +1,10 @@
+Src/main.o: ../Src/main.c ../Src/Std_Types.h ../Src/Compiler.h \
+ ../Src/Platform_Types.h ../Src/common_macros.h
+
+../Src/Std_Types.h:
+
+../Src/Compiler.h:
+
+../Src/Platform_Types.h:
+
+../Src/common_macros.h:
